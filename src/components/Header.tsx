0b8c8d21dc11acda7302e1c4bc035b4aee96f24a
John F <falveyjohnf@gmail.com>
2026-03-12 19:26:54 -0400

    fixed accidental global replace

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,11 +1,11 @@
 import { Helmet } from 'react-helmet';
-import { useQRScout-Rebuilt-RebuiltState } from '../store/store';
+import { useQRScout-RebuiltState } from '../store/store';
 
 export function Header() {
-  const page_title = useQRScout-Rebuilt-RebuiltState(state => state.formData.page_title);
+  const page_title = useQRScoutState(state => state.formData.page_title);
   return (
     <Helmet>
-      <title>QRScout-Rebuilt-Rebuilt | {page_title}</title>
+      <title>QRScout | {page_title}</title>
       <link rel="icon" href="/QRScout-Rebuilt/favicon.ico" />
     </Helmet>
   );
