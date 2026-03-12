122e14a510d44f6a339794aec1a670776376e538
John F <falveyjohnf@gmail.com>
2026-03-12 19:23:33 -0400

    Renamed lines to QRScout-Rebuilt

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,12 @@
 import { Helmet } from 'react-helmet';
-import { useQRScoutState } from '../store/store';
+import { useQRScout-Rebuilt-RebuiltState } from '../store/store';
 
 export function Header() {
-  const page_title = useQRScoutState(state => state.formData.page_title);
+  const page_title = useQRScout-Rebuilt-RebuiltState(state => state.formData.page_title);
   return (
     <Helmet>
-      <title>QRScout | {page_title}</title>
-      <link rel="icon" href="/QRScout/favicon.ico" />
+      <title>QRScout-Rebuilt-Rebuilt | {page_title}</title>
+      <link rel="icon" href="/QRScout-Rebuilt/favicon.ico" />
     </Helmet>
   );
 }
